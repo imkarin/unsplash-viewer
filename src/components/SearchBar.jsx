e02b150cabd60e2f1e472944b5c75a0a0387ecfc
Karin Meijvogel <karinmeijvogel@hotmail.com>
2022-04-29 17:11:55 +0200

    Add styling for SearchBar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledSearchBar = styled.form`
+  height: 56px;
+  background: transparent;
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+
+  > input {
+    height: 100%;
+    padding: 0 16px;
+
+    &[type=text] {
+      flex-grow: 1;
+      background-color: #fff;
+    }
+
+    &[type=submit] {
+      background-color: #e4e4e4;
+      text-transform: uppercase;
+    }
+  }
+`
 
 const SearchBar = () => {
   // Search bar will contain a small form with text field + button
@@ -9,10 +33,10 @@ const SearchBar = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <StyledSearchBar onSubmit={handleSubmit}>
       <input type='text' name='term'></input>
-      <input type='submit' />
-    </form>
+      <input type='submit' value='Search photos' />
+    </StyledSearchBar>
   )
 }
 
