08be9b2d5d3e06b383b8320a45cd3fa06ccc3740
Karin Meijvogel <karinmeijvogel@hotmail.com>
2022-04-29 16:56:52 +0200

    Added styling for Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,48 @@
 import React from 'react'
+import styled from 'styled-components'
 import ContentGrid from './styles/ContentGrid.styles'
+
+const StyledHeader = styled.header`
+  position: relative;
+  margin: 0;
+  width: 100vw;
+  height: 480px;
+  background-image: url(${(props) => props.featuredPhoto});
+  background-size: cover;
+  background-position: center;
+
+  // ContentGrid:
+  > div {
+    height: 100%;
+
+    h1 {
+      grid-row: 1;
+      grid-column: 1 / -1 ;
+      justify-self: center;
+      align-self: center;
+      margin-top: -36px;
+      text-align: center;
+    }
+    
+    img {
+      position: absolute;
+      top: 32px;
+    }
+  }
+
+  .white-block {
+    background: #fff;
+    grid-row: 1;
+    width: calc((100% - 1680px) / 2 + 206px);
+    height: 80px;
+    position: absolute;
+    bottom: 0;
+  
+    :last-of-type {
+      right: 0;
+    }
+  }
+`
 
 const Header = (props) => {
   // Header will show:
@@ -9,14 +52,17 @@ const Header = (props) => {
   // Searchbar
 
   return (
-    <header>
+    <StyledHeader featuredPhoto={props.featuredPhoto}>
       <ContentGrid>
-        <img src={props.logo} alt='' />
-        <img src={props.featuredPhoto} alt='' />
+        <img src={props.logo} alt='Unsplash viewer logo' />
         <h1>Find the perfect image for your design</h1>
         {/* Searchbar Component here */}
       </ContentGrid>
-    </header>
+
+      {/* Two white blocks: */}
+      <div className='white-block'></div>
+      <div className='white-block'></div>
+    </StyledHeader>
   )
 }
 
