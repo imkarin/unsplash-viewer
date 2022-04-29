f5cd4ad6f1ef72cecb83f3d0c2736e77a2cb4cf4
Karin Meijvogel <karinmeijvogel@hotmail.com>
2022-04-29 18:05:17 +0200

    Create searchPhotos and handleSearch functions to fetch photos from unsplash API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import logo from './assets/logo.png'
@@ -7,15 +7,31 @@ import theme from './theme.json'
 
 import GlobalStyles from './components/styles/Global.styled.js';
 import Header from './components/Header';
-import SearchBar from './components/SearchBar';
+
+const CLIENT_ID = '0d54d7bf8f81c9ee80a75d9e1263fbb6b8267fad9d908e597b9f7c4f6bcdee23';
+const BASE_URL = 'https://api.unsplash.com/photos/'
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('')
+
+  async function searchPhotos() {
+    const res = await fetch(`${BASE_URL}?client_id=${CLIENT_ID}`)
+      .then(res => res.json())
+    return res
+  }
+
+  async function handleSearch() {
+    searchPhotos()
+      .then(photos => console.log(photos))
+  }
+
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
         <GlobalStyles />
 
         <Header logo={logo} featuredPhoto={featured} />
+        <button onClick={handleSearch}>aaa</button>
       </ThemeProvider>
     </div>
   );
