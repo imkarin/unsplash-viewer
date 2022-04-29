d9e26caa4339de15d5049bfb04948b9fceca8dbc
Karin Meijvogel <karinmeijvogel@hotmail.com>
2022-04-29 19:27:20 +0200

    Add styling and hover effect to PhotoThumb

--- src/components/PhotoThumb.jsx
+++ src/components/PhotoThumb.jsx
@@ -1,11 +1,61 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledPhotoThumb = styled.article`
+  position: relative;
+
+  > .overlay {
+    position: absolute;
+    bottom: 4px;
+    left: 0;
+    padding: 16px;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    align-items: end;
+    background: linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0) 100%);
+    opacity: 0;
+    transition: 0.3s ease-in-out;
+    cursor: pointer;
+
+    > .user {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      gap: 12px;
+
+      > img {
+        border-radius: 100%;
+      }
+    }
+  }
+
+  :hover {
+    > .overlay {
+      opacity: 1;
+    }
+  }
+`
 
 const PhotoThumb = ({photo}) => {
   return (
-    <img 
-      alt={photo.alt_description}
-      src={photo.urls.regular}
-    />
+    <StyledPhotoThumb>
+      <img 
+        alt={photo.alt_description}
+        src={photo.urls.regular}
+        />
+      
+      <div className='overlay'>
+        <div className='user'>
+          <img 
+            src={photo.user.profile_image.small}
+            alt={photo.user.name} />
+          <p className='body-smaller'>
+            {photo.user.name}
+          </p>
+        </div>
+      </div>
+    </StyledPhotoThumb>
   )
 }
 
